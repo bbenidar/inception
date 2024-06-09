7d003a85769574d2bb7f16732a1a357263ce8fcb
Brahim Benidar <bbenidar@e3r1p6.1337.ma>
2024-06-09 18:37:24 +0100

    layhdi staf dyal trez osf

--- WORDPRESS/conf/auto_config.sh
+++ WORDPRESS/conf/auto_config.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # Wait for MariaDB to be ready
-until mysqladmin ping -h"$WORDPRESS_DB_HOST" --silent; do
-    echo "Waiting for MariaDB to be ready..."
-    sleep 5
-done
+# until mysqladmin ping -h"$WORDPRESS_DB_HOST" --silent; do
+#     echo "Waiting for MariaDB to be ready..."
+#     sleep 5
+# done
 
 # Check if wp-config.php already exists
-if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
     # Configuration file doesn't exist, create it
     wp config create --allow-root \
         --dbname="$WORDPRESS_DB_NAME" \
@@ -15,7 +14,5 @@ if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
         --dbpass="$WORDPRESS_DB_PASSWORD" \
         --dbhost="$WORDPRESS_DB_HOST:3306" \  # Use TCP/IP connection with port 3306
         --path="/var/www/wordpress"
-else
-    echo "WordPress configuration file already exists, skipping configuration."
-fi
+
 
