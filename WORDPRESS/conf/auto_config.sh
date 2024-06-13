901f55490ce6bb8226c0941d69e0f49669d67d58
Brahim Benidar <bbenidar@e3r1p1.1337.ma>
2024-06-13 14:55:12 +0100

    ll

--- WORDPRESS/conf/auto_config.sh
+++ WORDPRESS/conf/auto_config.sh
@@ -1,24 +1,31 @@
 #!/bin/sh
 
-#check if wp-config.php exist
-if [ -f ./wp-config.php ]
-then
-    rm -rf wp-config.php
+#wait for mariadb to start
+if ! nc -z $MYSQL_HOSTNAME 3306; then
+    echo "Waiting for MariaDB to start..."
+    while ! nc -z $MYSQL_HOSTNAME 3306; do
+        sleep 0.1
+    done
+    echo "MariaDB started"
 fi
 
-if [ -f ./wp-config.php ]
-then
-    echo "wordpress already downloaded"
-else
+# check if wp-config.php exists
+# if [ -f ./wp-config.php ]; then
+#     rm -rf wp-config.php
+# fi
+
+if [ ! -f ./wp-config.php ]; then
     rm -rf ./*
-    #Download wordpress and all config file
+
+    # Download WordPress and all config files
     wget http://wordpress.org/latest.tar.gz
     tar xfz latest.tar.gz
     mv -f wordpress/* .
     rm -rf latest.tar.gz
     rm -rf wordpress
-    echo "wordpress downloaded"
-    # adite wp-config.php
+    echo "WordPress downloaded"
+
+    # Edit wp-config.php
     cp wp-config-sample.php wp-config.php
     sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config.php
     sed -i "s/username_here/$MYSQL_USER/g" wp-config.php
@@ -27,20 +34,19 @@ else
     sed -i "s/utf8/utf8mb4/g" wp-config.php
 
     # Update site URL using sed
-    sed -i "s|define('WP_HOME', '.*');|define('WP_HOME', 'https://bbenidar.42.fr');|g" wp-config.php
-    sed -i "s|define('WP_SITEURL', '.*');|define('WP_SITEURL', 'https://bbenidar.42.fr');|g" wp-config.php
+    # sed -i "s|define('WP_HOME', '.*');|define('WP_HOME', 'https://bbenidar.42.fr');|g" wp-config.php
+    # sed -i "s|define('WP_SITEURL', '.*');|define('WP_SITEURL', 'https://bbenidar.42.fr');|g" wp-config.php
 
     echo "---------------------------------------"
     cat wp-config.php
     echo "------------------------------------"
 
-      wp user create $WP_ADMIN_USER $WP_ADMIN_EMAIL --user_pass=$WP_ADMIN_PASSWORD --role=administrator --allow-root
+    # Create WordPress users
+    wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root 
+    wp user create $WP_ADMIN_USER $WP_ADMIN_EMAIL --user_pass=$WP_ADMIN_PASSWORD --role=administrator --allow-root
     wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=author --allow-root
 
-    # Skip the installation page
-    wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root 
-    wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=author --allow-root
+    # Install WordPress
 fi
 
 exec "$@"
-
