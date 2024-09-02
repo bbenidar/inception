20ff3fc11d3c9f69e4bc5b76e07f057f94ceebfd
Brahim Benidar <bbenidar@e3r10p8.1337.ma>
2024-09-02 12:16:09 +0100

    inception Docker

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
 
-mysql_install_db
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    echo "Initializing MariaDB..."
+    mysql_install_db --user=mysql --ldata=/var/lib/mysql
+fi
+
+
+mysqld_safe --bind-address=0.0.0.0 &
+
+
+sleep 5
 
 if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then 
     echo "Database already exists"
 else
 
     echo "Initializing MariaDB..."
-    service mariadb start
-    mysql_secure_installation <<_EOF_
-y
-$MYSQL_ROOT_PASSWORD
-$MYSQL_ROOT_PASSWORD
-y
-n
-y
-y
-_EOF_
 
-    mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "CREAT DATABASE $MYSQL_DATABASE;"
+
+    mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE $MYSQL_DATABASE;"
     echo "Granting privileges..."
     echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
 
@@ -27,12 +27,10 @@ _EOF_
     echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
 
     echo "Importing WordPress database..."
-    mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
+    # mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
 fi
-
 
 /etc/init.d/mysql stop
 
 
 exec "$@"
-
--- test.cpp
+++ test.cpp
@@ -9,42 +9,26 @@ using namespace std;
 
 void add_to_nums(vector<int> &nums)
 {
-    nums.push_back(1);
-    nums.push_back(1);
-    nums.push_back(1);
-    nums.push_back(2);
-    nums.push_back(2);
-    nums.push_back(2);
-    nums.push_back(2);
-    nums.push_back(2);
-    nums.push_back(3);
+    nums.push_back(100);
+    // nums.push_back(0);
+    // nums.push_back(1);
 }
 
 
 int main()
 {
-    vector<int> nums;
-    add_to_nums(nums);
-    vector<int> cpy;
-        map <int, int> mp;
-        int max = 0;
-        for (int i : nums)
-        {
-            if (mp.count(i))
-                mp[i]++;
-            else
-                mp[i] = 1;
-            if (mp[i] > max)
-                max = mp[i];
-        }
-        //print the map
-        cout << "the max is: " << mp.count(max) << endl;
-        // get the key of the biggest value
-        cout << "[ ";
-        for (auto i : mp)
-        {
-            cout << i.first << " : " << i.second << ", ";
-        }
-        // get the biggest value
+    vector<int> c;
+    add_to_nums(c);
+
+        int j = c.size();
+        cout << j << endl;
+        int r =0;
+        sort(c.begin(), c.end());
+           for(int f = 0;  f < c.size(); f++)
+           {
+                if ((j + 1) - (f + 1) >= f)
+                    r = f + 1;
+           }
+        cout << r << endl;
 
 }
