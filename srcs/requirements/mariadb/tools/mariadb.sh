4331b6f2df78a25c6cf35d9220417ed602008a04
Brahim Benidar <115226959+bbenidar@users.noreply.github.com>
2024-09-24 18:04:30 +0100

    Update mariadb.sh

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,38 +1,40 @@
 #!/bin/sh
 
+# Initialize MariaDB data directory
+mysql_install_db
 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-    echo "Initializing MariaDB..."
-    mysql_install_db --user=mysql --ldata=/var/lib/mysql
-fi
+# Start MariaDB service
+# /etc/init.d/mysql start
+mysql_install_db
 
-
-mysqld_safe --bind-address=0.0.0.0 &
-
-
-sleep 5
-
+# Check if the database exists
 if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then 
     echo "Database already exists"
 else
-
+    # Set root password and perform secure installation
     echo "Initializing MariaDB..."
+    service mariadb start
+    mysql_secure_installation <<_EOF_
+_EOF_
 
-
-    mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE $MYSQL_DATABASE;"
+    mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "CREAT DATABASE $MYSQL_DATABASE;"
+    # Add root user for remote connections and flush privileges
     echo "Granting privileges..."
     echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
 
+    # Create database and user for WordPress
     echo "Creating WordPress database and user..."
     echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
 
+    # Import WordPress SQL script
     echo "Importing WordPress database..."
     mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
 fi
 
-mysqladmin -uroot -p$MYSQL_ROOT_PASSWORD  shutdown
+# Stop the MariaDB service
 /etc/init.d/mysql stop
 
-
-
+# Execute any additional command specified
 exec "$@"
+
+
